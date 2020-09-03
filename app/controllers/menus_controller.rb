e9f28c3cd96aeefa05c2d2f32d9576564b821a9e
gure-y <y.8kp.gm@gmail.com>
2020-09-03 11:56:55 +0900

    投稿編集機能の設定

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -25,6 +25,20 @@ def show
   @menu = Menu.find(params[:id])
 end
 
+def edit
+  @menu = Menu.find(params[:id])
+end
+
+def update
+  @menu = Menu.find(params[:id])
+  if @menu.valid?
+    @menu.update(menu_params)
+    redirect_to user_path(current_user)
+  else
+    render :edit
+  end
+end
+
 private
 def menu_params
   params.require(:menu).permit(:title, :image, :url, :recipe).merge(user_id: current_user.id)
