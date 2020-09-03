57cfdd5e5690bc8745a130f091471cd76e15c780
gure-y <y.8kp.gm@gmail.com>
2020-09-03 15:21:08 +0900

    menusコントローラーへauthenticate_user!の設定

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,59 +1,63 @@
 class MenusController < ApplicationController
-def top
-  @menus = Menu.includes(:user).order("RAND()").limit(3)
-end
 
-def index
-  @menus = Menu.includes(:user).order("created_at DESC")
-end
+  before_action :authenticate_user!, except: [:top, :praise]
 
-def new
-  @menu = Menu.new
-end
-
-def create
-  @menu = Menu.new(menu_params)
-  if @menu.valid?
-    @menu.save
-    redirect_to user_path(current_user)
-  else
-    render :new
+    
+  def top
+    @menus = Menu.includes(:user).order("RAND()").limit(3)
   end
-end
 
-def show
-  @menu = Menu.find(params[:id])
-end
-
-def edit
-  @menu = Menu.find(params[:id])
-end
-
-def update
-  @menu = Menu.find(params[:id])
-  if @menu.valid?
-    @menu.update(menu_params)
-    redirect_to user_path(current_user)
-  else
-    render :edit
+  def index
+    @menus = Menu.includes(:user).order("created_at DESC")
   end
-end
 
-def destroy
-  @menu = Menu.find(params[:id])
-  if @menu.destroy
-    redirect_to user_path(current_user)
-  else
-    render :show
+  def new
+    @menu = Menu.new
   end
-end
 
-def praise
-end
+  def create
+    @menu = Menu.new(menu_params)
+    if @menu.valid?
+      @menu.save
+      redirect_to user_path(current_user)
+    else
+      render :new
+    end
+  end
 
-private
-def menu_params
-  params.require(:menu).permit(:title, :image, :url, :recipe).merge(user_id: current_user.id)
-end
+  def show
+    @menu = Menu.find(params[:id])
+  end
+
+  def edit
+    @menu = Menu.find(params[:id])
+  end
+
+  def update
+    @menu = Menu.find(params[:id])
+    if @menu.valid?
+      @menu.update(menu_params)
+      redirect_to user_path(current_user)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @menu = Menu.find(params[:id])
+    if @menu.destroy
+      redirect_to user_path(current_user)
+    else
+      render :show
+    end
+  end
+
+  def praise
+  end
+
+  private
+  def menu_params
+    params.require(:menu).permit(:title, :image, :url, :recipe).merge(user_id: current_user.id)
+  end
 
 end
