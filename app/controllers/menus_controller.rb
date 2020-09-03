83f2b8a71e184d393a3763b0d4029906bae5c401
gure-y <y.8kp.gm@gmail.com>
2020-09-03 10:44:05 +0900

    menusコントローラにnew,createアクション設定

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,6 +1,24 @@
 class MenusController < ApplicationController
 def top
-  
+end
+
+def new
+  @menu = Menu.new
+end
+
+def create
+  @menu = Menu.new(menu_params)
+  if @menu.valid?
+    @menu.save
+    redirect_to user_path(current_user)
+  else
+    render :new
+  end
+end
+
+private
+def menu_params
+  params.require(:menu).permit(:title, :image, :url, :recipe).merge(user_id: current_user.id)
 end
 
 end
